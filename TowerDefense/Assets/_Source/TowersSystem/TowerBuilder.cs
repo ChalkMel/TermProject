a3fc865dfdd5330f77b554355296e55b4d52d792
ChalkMel <melnikvv24@st.ithub.ru>
2026-09-22 11:55:40 +0300

    tweak: highlighter for choosen cell

--- TowerDefense/Assets/_Source/TowersSystem/TowerBuilder.cs
+++ TowerDefense/Assets/_Source/TowersSystem/TowerBuilder.cs
@@ -6,63 +6,78 @@ using UnityEngine.Tilemaps;
 
 namespace _Source.TowersSystem
 {
-    public class TowerBuilder : MonoBehaviour
+  public class TowerBuilder : MonoBehaviour
+  {
+    [SerializeField] private TowerMenu menu;
+    [SerializeField] private Credits credits;
+    [SerializeField] private Tilemap tilemap;
+    [SerializeField] private Camera cam;
+
+    [SerializeField] private SpriteRenderer highlighter;
+
+    private Vector3Int _pendingCell;
+    private bool _hasPendingCell;
+
+    private void Awake()
     {
-      [SerializeField] private TowerMenu menu;
-      [SerializeField] private Credits credits;
-      [SerializeField] private Tilemap tilemap;
-      [SerializeField] private Camera cam;
+      menu.TowerChosen += OnTowerChosen;
+      menu.Hide();
+    }
 
-      private Vector3Int _pendingCell;
-      private bool _hasPendingCell;
+    private void OnDestroy()
+    {
+      menu.TowerChosen -= OnTowerChosen;
+    }
 
-      private void Awake()
+    private void Update()
+    {
+      if (Input.GetMouseButtonDown(0))
       {
-        menu.TowerChosen += OnTowerChosen;
-        menu.Hide();
-      }
+        if (EventSystem.current.IsPointerOverGameObject()) return;
 
-      private void OnDestroy()
-      {
-        menu.TowerChosen -= OnTowerChosen;
-      }
+        Vector3 worldPos = cam.ScreenToWorldPoint(Input.mousePosition);
+        worldPos.z = 0;
 
-      private void Update()
-      {
-        if (Input.GetMouseButtonDown(0))
-        {
-          if (EventSystem.current.IsPointerOverGameObject()) return;
+        Vector3Int cellPos = tilemap.WorldToCell(worldPos);
 
-          Vector3 worldPos = cam.ScreenToWorldPoint(Input.mousePosition);
-          worldPos.z = 0;
-
-          Vector3Int cellPos = tilemap.WorldToCell(worldPos);
-
-          TryOpenMenuForCell(cellPos);
-        }
-      }
-
-      private void TryOpenMenuForCell(Vector3Int cellPos)
-      {
-        if (tilemap.GetTile(cellPos) != null) return;
-
-        _pendingCell = cellPos;
-        _hasPendingCell = true;
-
-        menu.Show();
-      }
-
-      private void OnTowerChosen(TowerConfig tower)
-      {
-        if (!_hasPendingCell) return;
-        
-        if (credits.TrySpendMoney(tower.Cost))
-        {
-          tilemap.SetTile(_pendingCell, tower.Tile);
-        }
-
-        _hasPendingCell = false;
-        menu.Hide();
+        TryOpenMenuForCell(cellPos);
       }
     }
+
+    private void TryOpenMenuForCell(Vector3Int cellPos)
+    {
+      if (tilemap.GetTile(cellPos) != null) return;
+
+      _pendingCell = cellPos;
+      _hasPendingCell = true;
+
+      ShowHighlighter(cellPos);
+      menu.Show();
+    }
+
+    private void OnTowerChosen(TowerConfig tower)
+    {
+      if (!_hasPendingCell) return;
+
+      if (credits.TrySpendMoney(tower.Cost))
+      {
+        tilemap.SetTile(_pendingCell, tower.Tile);
+      }
+
+      _hasPendingCell = false;
+      menu.Hide();
+    }
+
+    private void ShowHighlighter(Vector3Int cell)
+    {
+      if (highlighter == null) return;
+      highlighter.gameObject.SetActive(true);
+      highlighter.transform.position = tilemap.GetCellCenterWorld(cell);
+    }
+
+    private void HideHighlighter()
+    {
+      if (highlighter != null) highlighter.gameObject.SetActive(false);
+    }
   }
+}
